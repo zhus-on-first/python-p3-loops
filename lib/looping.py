6ea92776641644479026a65e48f0685b397ea6a1
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-11 14:49:50 -0700

    fun :-)

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+     # For numbers which are multiples of both three and five, return "FizzBuzz". 
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+     # For multiples of three, return "Fizz" instead of the number. 
+        elif num % 3 == 0:
+            print("Fizz")
+    # For the multiples of five, return "Buzz". 
+        elif num % 5 == 0:
+            print("Buzz")
+    # For all other numbers, just return the number itself.
+        else:
+            print(num)
